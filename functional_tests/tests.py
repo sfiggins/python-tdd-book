0015bb115bbbefdacce44a947755f2cc992250b4
Stephen Figgins <sfiggins@ku.edu>
2019-01-12 23:24:56 -0600

    add new URL + view for adding to existing lists. FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -108,12 +108,3 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explantory text to that effect.
-        self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied, she goes back to sleep
